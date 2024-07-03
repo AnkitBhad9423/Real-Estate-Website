55fe5ce10b640dda1971206b3d7b7a6ec109a0e9
ankitbhad <ankitbhad90223@gmail.com>
2024-07-03 15:13:38 +0530

    Header part upto Companies name is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/hero/Hero";
-
+import "./App.css";
+import Companies from "./components/Companies/Companies";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
+      <div>
+        <div className="white-gradeint" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -14,10 +16,41 @@ const Hero = () => {
             </h1>
           </div>
           <div className="flexColStart hero-description">
-            <span>find a variety pf properties that suit you very easily</span>
-            <span>Forget all difficulty in finding a residence for you</span>
+            <span className="secondaryText">
+              Find a variety pf properties that suit you very easily
+            </span>
+            <span className="secondaryText">
+              Forget all difficulty in finding a residence for you
+            </span>
           </div>
-          <div className="search-bar"></div>
+          <div className=" flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={28} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award Wining</span>
+            </div>
+          </div>
         </div>
         <div className="flexCenter hero-right">
           <div className="image-container">
