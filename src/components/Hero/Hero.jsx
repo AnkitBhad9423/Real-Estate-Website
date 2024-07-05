7cdc36c460f2fbed0485753aef1e3123e55b2c81
ankitbhad <ankitbhad90223@gmail.com>
2024-07-05 08:36:05 +0530

    Completed Project

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter   hero-container">
+        {/* left side */}
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle " />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}
+            >
               Discover <br />
               Most Suitable
               <br /> Property
-            </h1>
+            </motion.h1>
           </div>
           <div className="flexColStart hero-description">
             <span className="secondaryText">
@@ -53,9 +62,17 @@ const Hero = () => {
           </div>
         </div>
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: "0", opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "spring",
+            }}
+            className="image-container"
+          >
             <img src="./hero-image.png" alt="introductary image" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
